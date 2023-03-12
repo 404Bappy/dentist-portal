aa30140f9091a9e8701b83becd470b5d36bd1f8a
Bappy71 <19103076@iubat.edu>
2023-03-13 00:51:17 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='mx-5'>
+            <h2>This Is Home</h2>
+        </div>
+    );
+};
+
+export default Home;
