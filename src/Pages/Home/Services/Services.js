cec11f92b6dcead829c3c9e55c1142e1ecbe149f
Bappy71 <19103076@iubat.edu>
2023-03-14 01:24:50 +0600

    Initial commit

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -9,19 +9,19 @@ const Services = () => {
     const servicesData = [
         {
             id: 1,
-            name : 'Fluoride Treatment',
-            description: 'ajkgfjafsrngafsg asgslfhgs fdsfdslk;fds fhuAFDSNGVJF GAJDHSG',
+            name: 'Fluoride Treatment',
+            description: 'Fluoride joins the tooth structure when your teeth develop, thus strengthening the teeth enamel, making them less susceptible to bacteria and cavities. It slows and may even reverse the development of decays and cavities by harming the bacteria which causes the cavities',
             img: fluoride
         },
         {
             id: 2,
-            name : 'Cavity Filling',
+            name: 'Cavity Filling',
             description: 'ajkgfjafsrngafsg asgslfhgs fdsfdslk;fds fhuAFDSNGVJF GAJDHSG',
             img: cavity
         },
         {
             id: 3,
-            name : 'Teeth Whitening',
+            name: 'Teeth Whitening',
             description: 'ajkgfjafsrngafsg asgslfhgs fdsfdslk;fds fhuAFDSNGVJF GAJDHSG',
             img: whitening
         },
@@ -32,7 +32,7 @@ const Services = () => {
                 <h3 className='text-xl font-bold text-primary uppercase'>Our Services</h3>
                 <h2 className='text-3xl'>Services We Provide</h2>
             </div>
-            <div>
+            <div className='grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
                 {
                     servicesData.map(service => <Service
                         key={service.id}
