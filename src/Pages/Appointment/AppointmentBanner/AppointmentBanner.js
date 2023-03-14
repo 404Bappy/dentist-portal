df78ed7b60e0e1a9d5c32277e126e4c48bcbeb66
Bappy71 <19103076@iubat.edu>
2023-03-15 03:21:59 +0600

    Initial commit

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import chair from '../../../assets/images/chair.png';
 
 const AppointmentBanner = () => {
     return (
         <header>
-            
+            <div className="hero ">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src={chair} alt="" className="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
         </header>
     );
 };
