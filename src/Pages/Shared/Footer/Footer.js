117ce84e4c45dc33d0993ae68530227cbb7a0022
Bappy71 <19103076@iubat.edu>
2023-03-15 02:20:04 +0600

    Initial commit

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,27 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <footer className="footer p-10">
-            <div>
-                <span className="footer-title">Services</span>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
+        <footer className=" p-10">
+            <div className='footer'>
+                <div >
+                    <span className="footer-title">Services</span>
+                    <Link to="/" className="link link-hover">Branding</Link>
+                    <Link to="/" className="link link-hover">Design</Link>
+                    <Link to="/" className="link link-hover">Marketing</Link>
+                    <Link to="/" className="link link-hover">Advertisement</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <Link to="/" className="link link-hover">About us</Link>
+                    <Link to="/" className="link link-hover">Contact</Link>
+                    <Link to="/" className="link link-hover">Jobs</Link>
+                    <Link to="/" className="link link-hover">Press kit</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <Link to="/" className="link link-hover">Terms of use</Link>
+                    <Link to="/" className="link link-hover">Privacy policy</Link>
+                    <Link to="/" className="link link-hover">Cookie policy</Link>
+                </div>
             </div>
-            <div>
-                <span className="footer-title">Company</span>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
-            </div>
-            <div>
-                <span className="footer-title">Legal</span>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
+            <div className='text-center mt-32'>
+                <p>Copyright © 2023 - All right reserved by Dentist Association Ltd</p>
             </div>
         </footer>
     );
