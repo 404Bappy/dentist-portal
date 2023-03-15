b721f22e43539b8f96384af98ccb2f46ecc4c4a1
Bappy71 <19103076@iubat.edu>
2023-03-15 16:04:24 +0600

    Initial commit

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import chair from '../../../assets/images/chair.png';
+import { DayPicker } from 'react-day-picker';
+
 
 const AppointmentBanner = () => {
     return (
@@ -8,7 +10,7 @@ const AppointmentBanner = () => {
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src={chair} alt="" className="max-w-sm rounded-lg shadow-2xl" />
                     <div>
-                        
+
                     </div>
                 </div>
             </div>
