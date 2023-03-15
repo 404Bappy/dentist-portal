cb86596236577816a9ae0697b8b3260f83c3e6bf
Bappy71 <19103076@iubat.edu>
2023-03-16 01:19:30 +0600

    Initial commit

---
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AvailableAppointments = () => {
+    return (
+        <div>
+            <p>Available Appointment on April 30, 2022</p>
+        </div>
+    );
+};
+
+export default AvailableAppointments;
