aa4b949a22f3d0b14a3b2ef1596ca7250c1f4c28
Bappy71 <19103076@iubat.edu>
2023-03-16 22:26:53 +0600

    Initial commit

--- src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
+++ src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
@@ -4,12 +4,12 @@ const AppointmentOption = ({ appointmentOptions }) => {
     const { name, Slots } = appointmentOptions;
     return (
         <div className="card  shadow-xl">
-            <div className="card-body text-center">
-                <h2 className="card-title text-primary">{name}</h2>
-                <p>{Slots.length > 0 ? Slots[0]: 'Try Another Day'}</p>
-                <p>{Slots.length} {Slots.length >1 ? 'Spaces' : 'Space'} Available</p>
+            <div className="card-body text-center ">
+                <h2 className=" text-2xl font-bold  text-secondary text-center">{name}</h2>
+                <p>{Slots.length > 0 ? Slots[0] : 'Try Another Day'}</p>
+                <p>{Slots.length} {Slots.length > 1 ? 'Spaces' : 'Space'} Available</p>
                 <div className="card-actions justify-center">
-                    <button className="btn btn-primary">Book Appointment</button>
+                    <button className="btn btn-primary text-white font-bold">Book Appointment</button>
                 </div>
             </div>
         </div>
--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -12,9 +12,9 @@ const AvailableAppointments = ({ selectedDate }) => {
             .then(data => setAppointmentOptions(data))
     }, [])
     return (
-        <section className='mt-16'>
+        <section className='my-16'>
             <p className='text-center text-secondary font-bold'>Available Appointment on {format(selectedDate, 'PP')}</p>
-            <div className='grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '>
+            <div className='grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
                 {
                     appointmentOptions.map(option => <AppointmentOption
                     key={option._id}
