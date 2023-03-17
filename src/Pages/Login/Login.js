2b734145d412fee45d2fc0d03e427cd8a13b9c33
Bappy71 <19103076@iubat.edu>
2023-03-18 04:33:41 +0600

    Initial commit

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
 
 const Login = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
+    const { signIn } = useContext(AuthContext);
+    const [loginError, setLoginError] = useState('');
+
     const handleLogin = data => {
         console.log(data);
+        setLoginError('');
+
+        signIn(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+            })
+            .catch(error => {
+                console.log(error.message)
+                setLoginError(error.message);
+            });
     }
 
     return (
@@ -33,6 +48,9 @@ const Login = () => {
 
 
                     <input className='btn btn-accent w-full' value="Login" type="submit" />
+                    <div>
+                        {loginError && <p className='text-red-600'>{loginError}</p>}
+                    </div>
                 </form>
                 <p>New to Dentist Portal <Link className='text-secondary' to="/signup">Create a new account</Link></p>
                 <div className="divider">OR</div>
