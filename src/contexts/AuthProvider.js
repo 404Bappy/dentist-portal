3cacc6932683376344898f9f9bdce464bab3a2dc
Bappy71 <19103076@iubat.edu>
2023-03-18 05:14:11 +0600

    Initial commit

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,11 +1,13 @@
-import React, { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 import app from '../firebase/firebase.config'
 
 export const AuthContext = createContext();
 const auth = getAuth(app)
 
+
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
@@ -14,9 +16,19 @@ const AuthProvider = ({ children }) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('user observing');
+            setUser(currentUser);
+        });
+
+        return () => unsubscribe();
+    }, [])
+
     const authInfo = {
-        createUser, 
-        signIn
+        createUser,
+        signIn,
+        user
     }
     return (
         <AuthContext.Provider value={authInfo}>
