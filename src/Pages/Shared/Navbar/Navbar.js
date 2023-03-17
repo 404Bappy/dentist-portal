dfc735233b2649ade2719d64a024b3582c6f9dc9
Bappy71 <19103076@iubat.edu>
2023-03-18 05:53:04 +0600

    Initial commit

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -3,14 +3,19 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthProvider';
 
 const Navbar = () => {
-    const { user } = useContext(AuthContext);
+    const { user, logOut } = useContext(AuthContext);
+    const handleLogOut = ()=>{
+        logOut()
+        .then(() => {})
+        .catch(err => console.log(err));
+    }
     const menuItems = <>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/appointment">Appointment</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/reviews">Reviews</Link></li>
         {user?.uid ?
-            <li><Link to="/login">Sign Out</Link></li>
+            <li><button onClick={handleLogOut}>Sign Out</button></li>
             : <li><Link to="/login">Login</Link></li>}
     </>
     return (
