3ad4cee6e81af21bd23ba3805cc7b9744edeecf0
Bappy71 <19103076@iubat.edu>
2023-03-26 08:13:59 +0600

    Initial commit

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -1,11 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import Loading from '../../Shared/Loading/Loading';
 
 const AddDoctor = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
+
+
+    const { data: specialties, isLoading } = useQuery({
+        queryKey: ['specialty'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:9000/appointmentSpecialty`);
+            const data = await res.json();
+            return data;
+        }
+    })
+
+
     const handleAddDoctor = data => {
         console.log(data);
 
+    }
+    if (isLoading) {
+        return <Loading></Loading>
     }
     return (
         <div className='w-96 p-7'>
@@ -28,9 +45,13 @@ const AddDoctor = () => {
                 <div className="form-control w-full max-w-xs">
                     <label className="label"><span className="label-text">Speciality</span></label>
                     <select className="select select-bordered w-full max-w-xs">
-                        <option disabled selected>Pick a Specialty</option>
-                        <option>Han Solo</option>
-                        <option>Greedo</option>
+                        <option disabled selected>Please Select a Specialty</option>
+                        {
+                            specialties.map(specialty => <option key={specialty._id} value={specialty.name}
+                            >{specialty.name}</option>)
+                        }
+
+
                     </select>
                 </div>
 
