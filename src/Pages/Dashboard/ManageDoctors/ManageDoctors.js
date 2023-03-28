84e6636f1c50fd4caf75bbe43a7b9af37b524d71
Bappy71 <19103076@iubat.edu>
2023-03-28 20:42:36 +0600

    Initial commit

--- src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
@@ -1,9 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const ManageDoctors = () => {
+    const { data: doctors, } = useQuery({
+        queryKey: ['doctors'],
+        queryFn: async () => {
+            try {
+                const res = await fetch('http://localhost:9000/doctors', {
+                    headers: {
+                        auhorization: `bearer ${localStorage.getItem('accessToken')}`
+                    }
+                });
+                const data = await res.json();
+                return data;
+
+            }
+            catch (error) {
+
+            }
+        }
+    })
     return (
         <div>
-            
+            <h2 className='text-3xl font-bold text-red-600'>Manage Doctors: {doctors?.length}</h2>
         </div>
     );
 };
