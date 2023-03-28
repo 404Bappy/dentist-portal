950a66f982d78cbbf1f97c708ec894bd878ab4ac
Bappy71 <19103076@iubat.edu>
2023-03-28 23:23:27 +0600

    Initial commit

--- src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
@@ -23,6 +23,34 @@ const ManageDoctors = () => {
     return (
         <div>
             <h2 className='text-3xl font-bold text-red-600'>Manage Doctors: {doctors?.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Specialty</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors.map(doctor => <tr key={doctor._id}>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Quality Control Specialist</td>
+                                <td>Blue</td>
+                                <td>Blue</td>
+                                <td>Blue</td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
