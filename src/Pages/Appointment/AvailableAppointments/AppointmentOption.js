ba9c2310cff24b859dd602fc0eadfd3eb90e0b90
Bappy71 <19103076@iubat.edu>
2023-04-01 01:18:12 +0600

    Initial commit

--- src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
+++ src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const AppointmentOption = ({ appointmentOptions, setTreatment }) => {
-    const { name, Slots } = appointmentOptions;
+    const { name, price, Slots } = appointmentOptions;
     return (
         <div className="card  shadow-xl">
             <div className="card-body text-center ">
                 <h2 className=" text-2xl font-bold  text-secondary text-center">{name}</h2>
                 <p>{Slots.length > 0 ? Slots[0] : 'Try Another Day'}</p>
                 <p>{Slots.length} {Slots.length > 1 ? 'Spaces' : 'Space'} Available</p>
+                <p><small><span className='font-bold'>Price :</span> ${price}</small></p>
                 <div className="card-actions justify-center">
 
                     <label
