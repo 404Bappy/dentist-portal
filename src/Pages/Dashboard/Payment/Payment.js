fde98ae3d6731b76d75a14ad0286678a5ca21d7d
Bappy71 <19103076@iubat.edu>
2023-04-02 00:24:32 +0600

    Initial commit

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,6 +1,8 @@
+import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import CheckoutForm from './CheckoutForm';
 
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
@@ -15,6 +17,11 @@ const Payment = () => {
         <div>
             <h3 className='text-3xl mt-6'>Payment for {treatment}</h3>
             <p className="text-xl">Please Pay <strong> ${price}</strong> for your appointment on {appointmentDate} at {slot} </p>
+            <div className='w-96 my-12'>
+                <Elements stripe={stripePromise}>
+                    <CheckoutForm />
+                </Elements>
+            </div>
         </div>
     );
 };
